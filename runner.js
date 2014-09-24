a346fcbce779f979d7974862a850a4c45e0456b1
sherodtaylor <sherodtaylor@gmail.com>
2014-09-24 00:29:51 -0700

    Node REPL lesson

--- runner.js
+++ runner.js
@@ -1,13 +1,19 @@
 #!/usr/bin/env node
 
 var adventure = require('adventure');
-var shop = adventure('example-adventure');
+var shop = adventure('My First Module');
+var fs = require('fs');
+var path = require('path');
 
-var problems = [ 'dinosaurs', 'robots', 'wowsers' ];
-problems.forEach(function (prob) {
-  shop.add(prob, function() { 
-    return require('./problems/' + prob);
+fs.readdir(path.resolve(__dirname, 'problems'), function(err, files) {
+  files.forEach(function(file) {
+    var problem = require(path.resolve(__dirname, 'problems', file));
+
+    shop.add(problem.name, function() { 
+      return problem;
+    });
   });
+
+  shop.execute(process.argv.slice(2));
 });
 
-shop.execute(process.argv.slice(2));
